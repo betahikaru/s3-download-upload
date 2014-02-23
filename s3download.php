83fed155cc1d57fc9cc86da6a18ba04bfc4d99c7
betahikaru <dev.beta.hikaru2000@gmail.com>
2014-02-24 01:15:07 +0900

    Test for notfood.betahikaru.com, download

--- s3download.php
+++ s3download.php
@@ -3,49 +3,43 @@
  * SDKの読み込みとS3Clientインスタンスの生成
  */
 require_once("aws.phar");
+require_once("secret.php");
 
 use Aws\S3\S3Client;
-use \Aws\Common\Enum\Region;
+use Aws\Common\Enum\Region;
 
 // キー、シークレットキー、リージョンを指定
 $client = S3Client::factory(array(
-            'key' => 'Your Access Key',
-            'secret' => 'Your Secret Key',
+            'key' => $iam_key,
+            'secret' => $iam_pass,
             'region' => Region::AP_NORTHEAST_1));
 
 /**
  * バケット・キーを指定しgetObjectメソッドでファイルのデータを取得
  */
 // バケット名
-$bucket = "Your Bucket Name";
+$bucket = $s3_bucket;
 // ダウンロード対象のキー(ファイル識別子)
-$key = "test.jpg";
+$key = "images/generated/avator.jpg";
 
 $result = $client->getObject(array(
     'Bucket' => $bucket,
     'Key' => $key
         ));
 
-/**
- * 取得したデータのブラウザ出力
- */
+$filename = end(explode('/', $key));
 //ファイルサイズ
 $length = $result['ContentLength'];
 //ファイルポインタを先頭に戻し、ファイルを読み込む
 $result['Body']->rewind();
 $data = $result['Body']->read($length);
-//キーからファイル名だけ取り出す
-$filename = end(explode('/', $key));
-
-//ファイルダウンロード用のHTTPヘッダ
-//Content-Type
-header('Content-Type: application/octet-stream');
-//ファイル名
-$disposition = 'Content-Disposition: attachment; filename="' . $filename . '"';
-header($disposition);
-//Content-Length
-$contentlength = 'Content-Length: ' . $length;
-header($contentlength);
-echo $data;
+try {
+    $fp = fopen("./tmp/" . $filename, "w");
+    fwrite($fp, $data);
+    fclose($fp);
+    chmod("./tmp/" . $filename, 0755);
+    echo $filename;
+} catch(Exceptin $e) {
+}
 
 ?>
